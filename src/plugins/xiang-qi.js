bbc066515c5f30a8bde21db659eb9519b8271945
binhphung <binhphung18032000@gmail.com>
2022-07-05 14:15:29 +0700

    Updated 050722 2:15 PM

--- src/plugins/xiang-qi.js
+++ src/plugins/xiang-qi.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import XiangQi from "@/plugins/chess/app";
-import * as notify from "@/plugins/chess/notify";
-import Board from "@/plugins/chess/board";
+// import * as notify from "@/plugins/chess/notify";
+// import Board from "@/plugins/chess/board";
 
 if (process.client) {
   Vue.use(new XiangQi());
